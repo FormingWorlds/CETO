3d13538735161fe19b33e69db963e862c7291b6a
Jorick Lania <j.f.lania@student.rug.nl>
2025-02-11 15:08:38 +0100

    Implemented non-initial version of optimisation / objective function. Runs, but a test run gives a small deviation from the result in Young's model.

--- source/model.py
+++ source/model.py
@@ -12,16 +12,17 @@ except:
     from source.activity import get_activity
 
 
-def optimisationfunction_initial(D, Pstd=1.0, T_low=1300, T_num=200):
+def optimisationfunction_initial(D, initial_moles, Pstd=1.0, T_low=1300.0, T_num=200):
     """Function establishes and computes initial values of 29 equations in 29 variables, taken from input file D. The first 19 reactions are equilibrium reactions for the 19 basis reactions of the model.
        Reactions 20 - 26 constrain the mass balance of the elements (H, C, O, Si, Mg, Fe). Reactions 27, 28 and 29 set summing constraints on the mole fractions of melt, atmosphere and metal phases.
        Function returns values f1 - f29 of each equation.
        Parameters:
         D (dict)                : input dictionary containing model input. Function uses mole fractions of planet constituents, moles of atmosphere, melt and metal, surface temperature
                                 and core-mantle equilibration temperature.
+        initial_moles (dict)    : dictionary containing initial amount of moles for Si, Mg, O, C, H, Fe, Na in the system
         Pstd (float, optional)  : Pressure at standard state, chosen to be 1.0 bar by default (As described in Schlichting&Young (2022))
         T_low (float, optional) : Lower bound for temperature array over which thermodynamics are calculated, default is 1300 K
-        T_num (float, optional) : Number of points in temperature array over which Gibbs free energies are calculated, default is 200.
+        T_num (int, optional)   : Number of points in temperature array over which Gibbs free energies are calculated, default is 200.
        Returns:
         F (list)                : list containing initial values of each equation (length 29), in order."""
 
@@ -79,22 +80,71 @@ def optimisationfunction_initial(D, Pstd=1.0, T_low=1300, T_num=200):
     f19 = ln(D["SiH4_gas"]) + 0.5*ln(D["O2_gas"]) - ln(D["SiO_gas"]) - 2*ln(D["H2_gas"]) + \
         G["GRT19_T"] - 1.5*ln(P/Pstd)
     
-    ## Mass balance for elements H, C, O, Na, Mg, Si, Fe
-    f20 = 0.0
-    f21 = 0.0
-    f22 = 0.0
-    f23 = 0.0
-    f24 = 0.0
-    f25 = 0.0
-    f26 = 0.0
+    ## Mass balance for elements H, C, O, Na, Mg, Si, Fe, must be zero initially
+    f20 = initial_moles["Si"] - moles_in_system('Si', D)
+    f21 = initial_moles["Mg"] - moles_in_system('Mg', D)
+    f22 = initial_moles["O"] - moles_in_system('O', D)
+    f23 = initial_moles["Fe"] - moles_in_system('Fe', D)
+    f24 = initial_moles["H"] - moles_in_system('H', D)
+    f25 = initial_moles["Na"] - moles_in_system('Na', D)
+    f26 = initial_moles["C"] - moles_in_system('C', D)
 
     ## Summing constraint on mole fractions for gas, melt and metal phases
-    f27 = 1 - np.sum([D[i] for i in D if "_melt" in i and "moles" not in i and "wt" not in i])
-    f28 = 1 - np.sum([D[i] for i in D if "_metal" in i and "moles" not in i and "wt" not in i and "bool" not in i])
-    f29 = 1 - np.sum([D[i] for i in D if "_gas" in i and "moles" not in i and "wt" not in i])
+    f27 = 1 - np.sum([D[key] for key in D if "_melt" in key and "moles" not in key and "wt" not in key])
+    f28 = 1 - np.sum([D[key] for key in D if "_metal" in key and "moles" not in key and "wt" not in key and "bool" not in key])
+    f29 = 1 - np.sum([D[key] for key in D if "_gas" in key and "moles" not in key and "wt" not in key])
 
     F_initial = np.array([f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, 
              f17, f18, f19, f20, f21, f22, f23, f24, f25, f26, f27, f28, f29])
     
     return F_initial
 
+def optimisationfunction(D, initial_moles, w_gas, Pstd=1.0, T_low=1300.0, T_num=200):
+    wt_massbalance = w_gas*D["wt_massbalance"]
+    wt_summing = w_gas*D["wt_summing"]
+    wt_atm = w_gas*D["wt_atm"]
+    wt_solub = w_gas*D["wt_solub"]
+    wt_melt = w_gas*D["wt_melt"]
+    wt_evap = w_gas*D["wt_evap"]
+
+    P_guess = calculate_pressure(D)
+    P = (P_guess - D["P_penalty"]) / P_guess
+
+    ## Create list of initial number of moles for each element to ensure proper indexing when assigning weights later
+    nElements = [initial_moles["Si"], initial_moles["Mg"], initial_moles["O"], initial_moles["Fe"], initial_moles["H"],
+                 initial_moles["Na"], initial_moles["C"]]
+    wtm = 5.0                                                       # Extra weight factor for mass balance equations
+
+    F_ini = optimisationfunction_initial(D, initial_moles, Pstd=Pstd, T_low=T_low, T_num=T_num)
+    F_list = []                                                     # List to contain values for the 29 equations
+
+
+    ## Assign weights to all equations
+    for i in range(1,(len(F_ini)+1)): # Runs over a range of i in (1 - 29)
+        f_i_ini = F_ini[(i-1)]
+        if i in (1, 2, 3, 4, 5, 6, 7):                              # F1 - F7 are in-melt reactions
+            f_i = wt_melt*D[f"wt_f{i}"]*f_i_ini
+        elif i in (8, 9, 10, 19):                                   # F8, F9, F10 and F19 are atmospheric reactions
+            f_i = wt_atm*D[f"wt_f{i}"]*f_i_ini 
+        elif i in (11, 12, 13, 14):                                 # F11 - F14 are reactions for evaporation of melt
+            f_i = wt_evap*D[f"wt_f{i}"]*f_i_ini
+        elif i in (15, 16, 17, 18):                                 # F15 - F18 are solubility reactions
+            f_i = wt_solub*D[f"wt_f{i}"]*f_i_ini
+        elif i in (20, 21, 22, 23, 24, 25, 26):                     # Mass Balance Equations
+            f_i = wt_massbalance*D[f"wt_f{i}"]*(wtm/nElements[int(i-20)]) * f_i_ini
+        elif i in (27, 28, 29):                                     # Summing constraints on phases
+            f_i = wt_summing*D[f"wt_f{i}"] * f_i_ini
+        F_list.append(f_i)
+
+    F_unpenalized = np.array(F_list) #convert to array before applying penalties
+
+    ## Assign penalties
+    F = np.zeros(30)
+    F[0:19] = sigmoidal_penalty(F_unpenalized[0:19], 0, 5, 1, 10000) #value=0, sharpness=5, tolerance=1, magnitude=10,000
+    F[19:26] = sigmoidal_penalty(F_unpenalized[19:26], 0, 1, 0.01, 1000) #value=0, sharpness=1, tolerance=0.01, magnitude=1,000
+    F[26:29] = sigmoidal_penalty(F_unpenalized[26:29], 0, 1, 0.005, 100000) #value=0, sharpness=1, tolerance=0.005, magnitude=100,000
+    F[29] = sigmoidal_penalty(P, 0, 1, 0.2, D["P_penalty"]) #value=0, sharpness=1, tolerance=0.2, take magnitude from input
+
+    ## Sum of squared errors in F
+    sum = np.sum((F**2))
+    return sum
--- source/testscript.py
+++ source/testscript.py
@@ -5,38 +5,35 @@ from constants import *
 
 from thermodynamics import shomate, get_Gibbs, calculate_GRT
 from activity import get_activity
-from model import optimisationfunction_initial
+from model import *
 from utilities import *
 
 ## Reading model input from config file
 sourcedir = Path(__file__).parent
 path_to_config = sourcedir / 'defaultconfig.txt'
 D = readconfig(path_to_config)
-D2 = readconfig(path_to_config)
 
-D2["bool_nonideal_mixing"] = False
+nSi = moles_in_system('Si', D)
+nMg = moles_in_system('Mg', D)
+nNa = moles_in_system('Na', D)
+nFe = moles_in_system('Fe', D)
+nC = moles_in_system('C', D)
+nH = moles_in_system('H', D)
+nO = moles_in_system('O', D)
 
-# activitylist = ["lngSi", "lngO", "lngH2", "lngH2O_melt", "lngH_metal", "xB"]
-# activity_nonideal = get_activity(D, get_all=True)
-# activity_nonideal_notall = get_activity(D, get_all=False)
-# activity_ideal = get_activity(D2)
+moles_initial = {'Si' : nSi,
+                 'Mg' : nMg,
+                 'Fe' : nFe,
+                 'Na' : nNa,
+                 'H'  : nH,
+                 'O'  : nO,
+                 'C'  : nC}
 
-# print("Activities for only ideal mixing:")
-# for i in range(len(activity_ideal)):
-#     print(f"{activitylist[i]} = {activity_ideal[i]}")
+F_ini = optimisationfunction_initial(D, moles_initial)
+w_gas = 1.0/np.max(np.abs(F_ini[:19])) # Weight for thermodynamic equations from maximum objective function from the 19 reaction values
 
-# print("Activities for non-ideal mixing, not all equations:")
-# for i in range(len(activity_ideal)):
-#     print(f"{activitylist[i]} = {activity_nonideal_notall[i]}")
-
-# print("Activities for non-ideal mixing, all equations:")
-# for i in range(len(activity_ideal)):
-#     print(f"{activitylist[i]} = {activity_nonideal[i]}")
-
-Fs = optimisationfunction_initial(D)
-F_penalties = sigmoidal_penalty(Fs, 0, 5, 1, 10000)
-
-print(F_penalties)
+Value = optimisationfunction(D, moles_initial, w_gas)
+print(Value)
 
 
 
